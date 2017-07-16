75ffd6c41ecb20ac02e9d050c731683fed28e6d1
LarissaWanderley <larissa-mw@hotmail.com>
2017-07-16 00:08:33 -0300

    logdetalhe continuacao

--- Suporte/Controllers/PerfilController.cs
+++ Suporte/Controllers/PerfilController.cs
@@ -3,11 +3,12 @@ using Suporte.Dominio;
 using Suporte.Persistencia;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Fazendas.Controllers
+namespace Suporte.Controllers
 {
     public class PerfilController : Controller
     {
@@ -38,7 +39,7 @@ namespace Fazendas.Controllers
         [HttpPost]
         public ActionResult Gravar(Perfil perfil)
         {
-            int nuAcao = perfil.Id < 1 ? 1 : 2; 
+            int nuAcao = perfil.Id < 1 ? 1 : 2;
             if (nuAcao == 1)
             {
                 List<Perfil> existe = DBPerfil.PorNome(perfil.TxPerfil);
@@ -47,50 +48,69 @@ namespace Fazendas.Controllers
                     ModelState.AddModelError("perfil.NomeJaCadastrado", "Perfil já Cadastrado");
                 }
             }
-            
+
             if (ModelState.IsValid)
             {
+                //Perfil antigo = DBPerfil.GetById(perfil.Id);
+                //int idLogAnterior = antigo.IdLog;
+                //antigo.IdLog = perfil.IdLog;
+                //if (nuAcao == 1 || !antigo.Equals(perfil))
+                //{
+                //    Log log = new Log();
+                //    log.NuAcao = nuAcao;
+                //    log.IdUsuario = 1;
+                //    log.IpUsuario = Request.UserHostAddress;
+                //    log.TxTabela = "Perfil";
+                //    log.DtTransacao = DateTime.Now;
+                //    log.TxUrl = Request.Url.AbsoluteUri;
+                //    log.IdLogAnterior = idLogAnterior;
+                //    //DbTransaction transacao = GetTransaction();
 
-             //   LogDetalhe logDetalheAnterior = DBLogDetalhe.PesqUltimo("Perfil", perfil.Id);
-
-                Perfil antigo = DBPerfil.GetById(perfil.Id);
-                int idLogAnterior = antigo.IdLog;
-                antigo.IdLog = perfil.IdLog;
-
-                if (nuAcao == 1 || !antigo.Equals(perfil))
-                    {
-                    DBPerfil.Save(perfil);
-                    Log log = new Log();
-                    log.IdChave = perfil.Id;
-                    log.NuAcao = nuAcao;
-                    log.IdUsuario = 1;
-                    log.IpUsuario = Request.UserHostAddress;
-                    log.TxTabela = "Perfil";
-                    log.DtTransacao = DateTime.Now;
-                    log.TxUrl = Request.Url.AbsoluteUri;
-                    log.IdLogAnterior = idLogAnterior;
-                    DBLog.Save(log);
-                    perfil.IdLog = log.Id;
-                    DBPerfil.Save(perfil);
-
-                  //  
-                    if (idLogAnterior > 0)
-                        {
-                            antigo.IdLog = idLogAnterior;
-                      
-                            LogDetalhe logDetalhe = new LogDetalhe();
-                            logDetalhe.IdLog = idLogAnterior;
-                            logDetalhe.TxObjeto = JsonConvert.SerializeObject(antigo);
-                            DBLogDetalhe.Save(logDetalhe);
-                        }
-                    }             
-                //  string aa = JsonConvert.SerializeObject(log);
-                //  Log p1 = JsonConvert.DeserializeObject<Log>(aa);
-                //  ou  
-                //  Log p = new Log();
-                //  JsonConvert.PopulateObject(aa, p);
-
-                return RedirectToAction("Index");
+                //    try
+                //    {
+                //        DBPerfil.Save(perfil);
+                //        //DBPerfil.Save(perfil, transacao);
+                //        log.IdChave = perfil.Id;
+                //        DBLog.Save(log);
+                //        //   DBLog.Save(log, transacao);
+                //        perfil.IdLog = log.Id;
+                //        DBPerfil.Save(perfil);
+                //        //   DBPerfil.Save(perfil,transacao);
+                //        if (idLogAnterior > 0)
+                //        {
+                //            LogDetalhe logDetalhe = new LogDetalhe();
+                //            logDetalhe.IdLog = idLogAnterior;
+                //            antigo.IdLog = idLogAnterior;
+                //            logDetalhe.TxObjeto = JsonConvert.SerializeObject(antigo);
+                //            DBLogDetalhe.Save(logDetalhe);
+                //            // DBLogDetalhe.Save(logDetalhe, transacao);
+                //        }
+                //        // transacao.Commit();
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        // if (transacao.Connection != null)
+                //        //{
+                //        //   transacao.Rollback();
+                //        ModelState.AddModelError("perfil.ErroGrava", "Erro na Gravação do Registro");
+                //        ViewBag.Perfil = perfil;
+                //        return View("Form");
+                //        //}
+                //    }
+                //}
+                string ipUsuario = Request.UserHostAddress;
+                string txUrl = Request.Url.AbsoluteUri;
+                if (DBPerfil.Sava(perfil , ipUsuario, txUrl))
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Perfil = perfil;
+                    return View("Form");
+                }
+                   
+               
             }
             else
             {
@@ -98,31 +118,45 @@ namespace Fazendas.Controllers
                 return View("Form");
             }
         }
-      
+
+        //  string aa = JsonConvert.SerializeObject(log);
+        //  Log p1 = JsonConvert.DeserializeObject<Log>(aa);
+        //  ou  
+        //  Log p = new Log();
+        //  JsonConvert.PopulateObject(aa, p);
+
         public ActionResult Excluir(int id)
         {
-            Log log = new Log();
-            log.IdChave = id;
-            log.NuAcao = 3;
-            log.IdUsuario = 1;
-            log.IpUsuario = Request.UserHostAddress;
-            log.TxTabela = "Perfil";
-            log.DtTransacao = DateTime.Now;
-            log.TxUrl = Request.Url.AbsoluteUri;
-            log.IdLogAnterior = DBLog.PesqLogAnterior(log.TxTabela, log.IdChave);
-            DBLog.Save(log);
+            Perfil perfil = DBPerfil.GetById(id);
+            string ipUsuario = Request.UserHostAddress;
+            string txUrl = Request.Url.AbsoluteUri;
+            if (!DBPerfil.Excui(perfil, ipUsuario, txUrl))
+            {
+                ViewBag.Error = "Erro na Exclusão do Registro";
 
-            Perfil antigo = DBPerfil.GetById(id);
+            }
 
-            LogDetalhe logDetalhe = new LogDetalhe();
-            logDetalhe.IdLog = id;
-            logDetalhe.TxObjeto = JsonConvert.SerializeObject(antigo);
-            DBLogDetalhe.Save(logDetalhe);
+           //Log log = new Log();
+            //log.IdChave = id;
+            //log.NuAcao = 3;
+            //log.IdUsuario = 1;
+            //log.IpUsuario = Request.UserHostAddress;
+            //log.TxTabela = "Perfil";
+            //log.DtTransacao = DateTime.Now;
+            //log.TxUrl = Request.Url.AbsoluteUri;
+            //log.IdLogAnterior = DBLog.PesqLogAnterior(log.TxTabela, log.IdChave);
+            //DBLog.Save(log);
 
-            DBPerfil.Delete(id);
+            //Perfil antigo = DBPerfil.GetById(id);
+
+            //LogDetalhe logDetalhe = new LogDetalhe();
+            //logDetalhe.IdLog = id;
+            //logDetalhe.TxObjeto = JsonConvert.SerializeObject(antigo);
+            //DBLogDetalhe.Save(logDetalhe);
+
+            //DBPerfil.Delete(id);
+
             return RedirectToAction("Index");
-
-
         }
     }
 }
--- Suporte/Persistencia/DBPerfil.cs
+++ Suporte/Persistencia/DBPerfil.cs
@@ -1,4 +1,5 @@
-﻿using Suporte.Dominio;
+﻿using Newtonsoft.Json;
+using Suporte.Dominio;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
@@ -21,5 +22,95 @@ namespace Suporte.Persistencia
         inner join tblog l on p.IdPerfil = l.IdChave  and l.TxTabela = 'perfil'
         inner join tbLogDetalhe ld on l.IdLog = ld.IdLog
         order by p.IdPerfil, l.IdLog*/
+
+
+        public static bool Sava(Perfil perfil, string ipUsuario, string txUrl)
+        {
+            int nuAcao = perfil.Id < 1 ? 1 : 2;
+            Perfil antigo = DBPerfil.GetById(perfil.Id);
+            int idLogAnterior = antigo.IdLog;
+            //antigo.IdLog = 0;
+            //perfil.IdLog = 0;
+            //if (nuAcao == 1 || ! antigo.Equals(perfil))
+            if (nuAcao == 1 || antigo.Id != perfil.Id || antigo.IdLog != perfil.IdLog || antigo.TxPerfil != perfil.TxPerfil )
+            {
+                Log log = new Log();
+                log.NuAcao = nuAcao;
+                log.IdUsuario = 1;
+                log.IpUsuario = ipUsuario;
+                log.TxTabela = "Perfil";
+                log.DtTransacao = DateTime.Now;
+                log.TxUrl = txUrl;
+                log.IdLogAnterior = idLogAnterior;
+                //DbTransaction transacao = GetTransaction();
+                try
+                {
+                    DBPerfil.Save(perfil);
+                    //DBPerfil.Save(perfil, transacao);
+                    log.IdChave = perfil.Id;
+                    DBLog.Save(log);
+                    //   DBLog.Save(log, transacao);
+                    perfil.IdLog = log.Id;
+                    DBPerfil.Save(perfil);
+                    //   DBPerfil.Save(perfil,transacao);
+                    if (idLogAnterior > 0)
+                    {
+                        LogDetalhe logDetalhe = new LogDetalhe();
+                        logDetalhe.IdLog = idLogAnterior;
+                        antigo.IdLog = idLogAnterior;
+                        logDetalhe.TxObjeto = JsonConvert.SerializeObject(antigo);//Log t = JsonConvert.DeserializeObject<Log>(l.txObjeto)
+                       
+                        DBLogDetalhe.Save(logDetalhe);
+                        // DBLogDetalhe.Save(logDetalhe, transacao);
+                    }
+                    return true;
+                    // transacao.Commit();
+                }
+                catch (Exception ex)
+                {
+                    // if (transacao.Connection != null)
+                    //{
+                    //   transacao.Rollback();
+                    return false;
+                    //}
+
+                }
+
+            }
+            return true;
+        }
+        public static bool Excui(Perfil perfil, string ipUsuario, string txUrl)
+        {
+            //DbTransaction transacao = GetTransaction();
+            try
+            {
+                LogDetalhe logDetalhe = new LogDetalhe();
+                logDetalhe.IdLog = perfil.IdLog;
+                logDetalhe.TxObjeto = JsonConvert.SerializeObject(perfil);
+                DBLogDetalhe.Save(logDetalhe);
+
+                Log log = new Log();
+                log.IdChave = perfil.Id;
+                log.NuAcao = 3;
+                log.IdUsuario = 1;
+                log.IpUsuario = ipUsuario;
+                log.TxTabela = "Perfil";
+                log.DtTransacao = DateTime.Now;
+                log.TxUrl = txUrl;
+                log.IdLogAnterior = perfil.IdLog;
+                DBLog.Save(log);
+
+                DBPerfil.Delete(perfil.Id);
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+
+           
+        }
     }
 }
+   
